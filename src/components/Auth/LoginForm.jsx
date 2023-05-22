33c3604e2a887da48b24f3e57427730cce868613
ElliotHarrison <ellioth1029@gmail.com>
2023-05-21 23:29:26 -0700

    adds style to sign up page, but needs link to direct user, and cursor position needs to be set in input fields with either js or nesting the element and using padding

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './AuthForms.module.css';
 
 const LoginForm = ({ handleLogin }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
--- src/components/Auth/SignupForm.jsx
+++ src/components/Auth/SignupForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./AuthForms.module.css";
 
 const SignupForm = ({ handleSignup }) => {
   const [userFormData, setUserFormData] = useState({
@@ -53,68 +54,95 @@ const SignupForm = ({ handleSignup }) => {
   }
 
   return (
+  <div>
+    <h1 className={styles.header}>Sign Up</h1>
     <form onSubmit={handleSubmit}>
-      <label htmlFor="user-email">Email: </label>
-      <input
-        type="text"
-        name="user-email"
-        value={userFormData.email}
-        onChange={handleChange}
-      />
-      <label htmlFor="user-password">Password: </label>
-      <input
-        type="text"
-        name="user-password"
-        value={userFormData.password}
-        onChange={handleChange}
-      />
-      <label htmlFor="user-rePassword">Re-enter Password</label>
-      <input
-        type="text"
-        name="user-rePassword"
-        value={userFormData.rePassword}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="profile-username">Username: </label>
-      <input
-        type="text"
-        name="profile-username"
-        value={profileFormData.username}
-        onChange={handleChange}
-      />
-      <label htmlFor="profile-firstName">First Name: </label>
-      <input
-        type="text"
-        name="profile-firstName"
-        value={profileFormData.firstName}
-        onChange={handleChange}
-      />
-      <label htmlFor="profile-lastName">Last Name: </label>
-      <input
-        type="text"
-        name="profile-lastName"
-        value={profileFormData.lastName}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="profile-role"></label>
-      <select
-        name="profile-role"
-        value={profileFormData.role}
-        onChange={handleChange}
-      >
-        <option value="homeless">Homeless</option>
-        <option value="samaritan">Samaritan</option>
-        <option value="clinic">Clinic</option>
-        <option value="shelter">Shelter</option>
-        <option value="vendor">Vendor</option>
-        <option value="government">Government</option>
-        <option value="business">Business</option>
-      </select>
-
-      <button type="submit">SIGN-UP</button>
+      <div className={styles.fieldsContainer}>
+        <input
+          type="text"
+          placeholder="FIRST NAME"
+          name="profile-firstName"
+          value={profileFormData.firstName}
+          onChange={handleChange}
+        />
+      
+      
+        
+        <input
+          type="text"
+          placeholder="LAST NAME"
+          name="profile-lastName"
+          value={profileFormData.lastName}
+          onChange={handleChange}
+        />
+      
+      
+        
+        <input
+          type="text"
+          placeholder="EMAIL ADDRESS *"
+          
+          name="user-email"
+          value={userFormData.email}
+          onChange={handleChange}
+        />
+      
+      
+        
+        <input
+          type="text"
+          placeholder="PASSWORD"
+          name="user-password"
+          value={userFormData.password}
+          onChange={handleChange}
+        />
+      
+      
+        
+        <input
+          type="text"
+          placeholder="RETYPE PASSWORD"
+          name="user-rePassword"
+          value={userFormData.rePassword}
+          onChange={handleChange}
+        />
+      
+      
+        
+        <input
+          type="text"
+          placeholder="USERNAME *"
+          name="profile-username"
+          value={profileFormData.username}
+          onChange={handleChange}
+        />
+      </div>
+      
+      
+      
+     
+      {/* <div className={styles.fieldContainer}>
+        <div className={styles.selectMenuContainer}>
+          <select
+            name="profile-role"
+            value={profileFormData.role}
+            onChange={handleChange}
+          >
+            <option value="homeless">Homeless</option>
+            <option value="samaritan">Samaritan</option>
+            <option value="clinic">Clinic</option>
+            <option value="shelter">Shelter</option>
+            <option value="vendor">Vendor</option>
+            <option value="government">Government</option>
+            <option value="business">Business</option>
+          </select>
+        </div>
+      </div> */}
+      
+        <button className={styles.submitButton}type="submit">Sign Up</button>
+     
     </form>
+  </div>
   );
 };
 
